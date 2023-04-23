c06bc2e36f9cb4f1e0c2d752da50bff96b5a5071
vdBurgIT <122352124+vdBurgIT@users.noreply.github.com>
2023-04-23 12:45:25 +0200

    Create MWP_SCRIPT_Win10_RemoveWin10apps.ps1

---
+++ MWP_SCRIPT_Win10_RemoveWin10apps.ps1
@@ -0,0 +1,95 @@
+# Uninstall 3D Builder:
+Get-AppxPackage  3dbuilder | Remove-AppxPackage
+
+# Uninstall Alarms and Clock:
+Get-AppxPackage  windowsalarms | Remove-AppxPackage
+
+# Uninstall Calendar and Mail:
+Get-AppxPackage  windowscommunicationsapps | Remove-AppxPackage
+
+# Uninstall Get Office:
+Get-AppxPackage  officehub | Remove-AppxPackage
+
+# Uninstall Get Skype:
+Get-AppxPackage  skypeapp | Remove-AppxPackage
+
+# Uninstall Get Started:
+Get-AppxPackage  getstarted | Remove-AppxPackage
+
+# Uninstall Groove Music:
+Get-AppxPackage  zunemusic | Remove-AppxPackage
+
+# Uninstall Maps:
+Get-AppxPackage  windowsmaps | Remove-AppxPackage
+
+# Uninstall Microsoft Solitaire Collection:
+Get-AppxPackage  solitairecollection | Remove-AppxPackage
+
+# Uninstall Money:
+Get-AppxPackage  bingfinance | Remove-AppxPackage
+
+# Uninstall Movies & TV:
+Get-AppxPackage  zunevideo | Remove-AppxPackage
+
+# Uninstall News:
+Get-AppxPackage  bingnews | Remove-AppxPackage
+
+# Uninstall OneNote:
+Get-AppxPackage  onenote | Remove-AppxPackage
+
+# Uninstall Sports:
+Get-AppxPackage  bingsports | Remove-AppxPackage
+
+# Uninstall Voice Recorder:
+Get-AppxPackage  soundrecorder | Remove-AppxPackage
+
+# Uninstall Weather:
+Get-AppxPackage  bingweather | Remove-AppxPackage
+
+# Uninstall Xbox:
+Get-AppxPackage  xboxapp | Remove-AppxPackage
+
+#Uninstall Non-Microsoft:
+Get-AppxPackage  king.com.CandyCrushSodaSaga | Remove-AppxPackage
+Get-AppxPackage  king.com.* | Remove-AppxPackage
+Get-AppxPackage  ClearChannelRadioDigital.iHeartRadio | Remove-AppxPackage
+Get-AppxPackage  4DF9E0F8.Netflix | Remove-AppxPackage
+Get-AppxPackage  6Wunderkinder.Wunderlist | Remove-AppxPackage
+Get-AppxPackage  Drawboard.DrawboardPDF | Remove-AppxPackage
+Get-AppxPackage  2FE3CB00.PicsArt-PhotoStudio | Remove-AppxPackage
+Get-AppxPackage  D52A8D61.FarmVille2CountryEscape | Remove-AppxPackage
+Get-AppxPackage  TuneIn.TuneInRadio | Remove-AppxPackage
+Get-AppxPackage  GAMELOFTSA.Asphalt8Airborne | Remove-AppxPackage
+Get-AppxPackage  TheNewYorkTimes.NYTCrossword | Remove-AppxPackage
+Get-AppxPackage  DB6EA5DB.CyberLinkMediaSuiteEssentials | Remove-AppxPackage
+Get-AppxPackage  Facebook.Facebook | Remove-AppxPackage
+Get-AppxPackage  flaregamesGmbH.RoyalRevolt2| Remove-AppxPackage
+Get-AppxPackage  Playtika.CaesarsSlotsFreeCasino| Remove-AppxPackage
+Get-AppxPackage  A278AB0D.MarchofEmpires | Remove-AppxPackage
+Get-AppxPackage  KeeperSecurityInc.Keeper | Remove-AppxPackage
+Get-AppxPackage  ThumbmunkeysLtd.PhototasticCollage | Remove-AppxPackage
+Get-AppxPackage  XINGAG.XING | Remove-AppxPackage
+Get-AppxPackage  89006A2E.AutodeskSketchBook| Remove-AppxPackage
+Get-AppxPackage  D5EA27B7.Duolingo-LearnLanguagesforFree | Remove-AppxPackage
+Get-AppxPackage  46928bounde.EclipseManager | Remove-AppxPackage
+Get-AppxPackage  ActiproSoftwareLLC.562882FEEB491 | Remove-AppxPackage
+Get-AppxPackage  HP.ePrint.HPePrint | Remove-AppxPackage
+Get-AppxPackage  Microsoft.BingFoodAndDrink | Remove-AppxPackage
+Get-AppxPackage  Microsoft.BingTravel | Remove-AppxPackage
+Get-AppxPackage  Microsoft.BingHealthAndFitness | Remove-AppxPackage
+Get-AppxPackage  Microsoft.WindowsReadingList | Remove-AppxPackage
+Get-AppxPackage  AD2F1837.HPJumpStart | Remove-AppxPackage
+Get-AppxPackage  Microsoft.DesktopAppInstaller | Remove-AppxPackage
+Get-AppxPackage  Microsoft.Microsoft3DViewer | Remove-AppxPackage
+Get-AppxPackage  Microsoft.MicrosoftStickyNotes | Remove-AppxPackage
+Get-AppxPackage  Microsoft.Wallet | Remove-AppxPackage
+Get-AppxPackage  Microsoft.XboxGameOverlay | Remove-AppxPackage
+Get-AppxPackage  Microsoft.XboxIdentityProvider | Remove-AppxPackage
+Get-AppxPackage  Microsoft.XboxSpeechToTextOverlay | Remove-AppxPackage
+Get-AppxPackage  Microsoft.FreshPaint | Remove-AppxPackage
+Get-AppxPackage  Microsoft.Office.OneNote | Remove-AppxPackage
+Get-AppxPackage  Microsoft.Office.Sway | Remove-AppxPackage 
+Get-AppxPackage  Microsoft.SkypeApp | Remove-AppxPackage
+Get-AppxPackage  Microsoft.Advertising.Xaml | Remove-AppxPackage
+Get-AppxPackage  Microsoft.Getstarted | Remove-AppxPackage
+Get-AppxPackage  AdobeSystemsIncorporated.AdobePhotoshopExpress | Remove-AppxPackage
